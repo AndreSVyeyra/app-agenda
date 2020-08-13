f183f92d2644ad261a86035d6847a7f549fe3204
Airton <airtonfrance@gmail.com>
2020-08-13 00:17:54 -0400

    Adicionando lógica e estilos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import {MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import "./index.css";
 import Event from './Event.js';
 
@@ -10,6 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal: false,
       events: [
         {
           id: 1,
@@ -27,7 +28,8 @@ class App extends Component {
         { 
           id: 3, 
           time: "08:00", 
-          title: "Titulo 03" 
+          title: "Titulo 03",
+          description: "B"
         },
         {
           id: 4,
@@ -44,8 +46,10 @@ class App extends Component {
           <React.Fragment>
           <MDBContainer>
             <MDBRow>
-              <MDBCol md="9">
-              {this.state.events.map(event => (
+              <MDBCol md="9" className="mb-r">
+              <h2 className="text-uppercase my-3">Today:</h2>
+              <div id="schedule-items">
+                {this.state.events.map(event => (
                   <Event
                     key={event.id}
                     id={event.id}
@@ -53,14 +57,70 @@ class App extends Component {
                     title={event.title}
                     location={event.location}
                     description={event.description}
+                    onDelete={this.handleDelete}
                   />
                 ))}
-              </MDBCol>
-              <MDBCol md="3" />
-            </MDBRow>
+              </div>
+              <MDBRow className="mb-4">
+                <MDBCol xl="3" md="6" className="mx-auto text-center">
+                  <MDBBtn color="green" rounded onClick={this.toggleModal}>
+                    Adicionar evento
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+            <MDBCol md="3">
+              <h3 className="text-uppercase my-3">CRONOGRAMA</h3>
+              <h6 className="my-3">
+                Atenção com o tempo. Você tem {" "}
+                <b>{this.state.events.length} eventos </b> hoje.
+              </h6>
+              <h2 className="my-3">
+                <MDBRow>
+                    <MDBCol xs="3" className="text-center">
+                      <MDBIcon icon="sun" fixed />
+                    </MDBCol>
+                    <MDBCol xs="9">Ensolarado</MDBCol>
+                  </MDBRow>
+                  <MDBRow>
+                    <MDBCol xs="3" className="text-center">
+                    <MDBIcon icon="thermometer-three-quarters" fixed />
+                    </MDBCol>
+                    <MDBCol xs="9">28°C</MDBCol>
+                  </MDBRow>
+              </h2>
+              <p>
+              Hoje vai estar seco e ensolarado, tornando-se 
+              quente à tarde com temperaturas entre 30 e 34 graus.
+              Proteja-se.
+              </p>
+            </MDBCol>
+          </MDBRow>
           </MDBContainer>
+          <MDBModal isOpen={this.state.modal} toggle={this.toggleModal}>
+          <MDBModalHeader
+            className="text-center color"
+            titleClass="w-100 font-weight-bold"
+            toggle={this.toggleModal}
+          >
+            Adicionar novo evento
+          </MDBModalHeader>
+          <MDBModalBody>Body</MDBModalBody>
+          <MDBModalFooter className="justify-content-center">
+          </MDBModalFooter>
+        </MDBModal>
           </React.Fragment>
       );
   }
+  handleDelete = eventId => {
+    const events = this.state.events.filter(e => e.id !== eventId);
+    this.setState({ events });
+  };
+  toggleModal = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
 }
+
 export default App;
--- src/Event.js
+++ src/Event.js
@@ -2,15 +2,38 @@ import React, { Component } from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import {MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
 import "./index.css";
 
 class Event extends Component {
   render() {
     return (
         <React.Fragment>
-            <h3>{this.props.time} - {this.props.title}</h3>
-            <h6>Location: {this.props.location}</h6>
-            <p>Desc: {this.props.description}</p>
+            <div className="media mt-1">
+                <h3 className="h3-responsive font-weight-bold mr-3">
+                {this.props.time}
+                </h3>
+            
+                <div className="media-body mb-3 mb-lg-3">
+                    <MDBBadge onClick={() => this.props.onDelete(this.props.id)} color="danger" className="ml-2 float-right">
+                    -
+                    </MDBBadge>
+                    <h6 className="mt-0 font-weight-bold">{this.props.title} </h6>{" "}
+                    <hr className="hr-bold my-2" />
+                    {this.props.location && (
+                        <React.Fragment>
+                            <p className="font-smaller mb-0">
+                            <MDBIcon icon="location-arrow" /> {this.props.location}
+                            </p>
+                        </React.Fragment>
+                    )}
+                </div>
+            </div>
+            {this.props.description && (
+                <p className="p-2 mb-4 lighten-5 lighten-5">
+                {this.props.description}
+                </p>
+            )}
         </React.Fragment>
     );
   }
